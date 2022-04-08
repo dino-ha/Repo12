438727920899b6f08d1de9566f306ebfc258d0d0
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:24:48 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,40 @@
+var collection = {
 
+    2548: {
+
+        album: "Slippery when wet",
+        artist: "Bon Jovi",
+        tracks: [
+
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    2468: {
+
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+
+        artist: "Robert Palmer",
+        tracks: [ ]
+    }
+};
+
+var oldCollection = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(object, id, prop, value){
+
+    collection[object][id][prop] = collection.push(value);
+    return collection;
+}
+
+console.log(updateRecords(collection, id, prop, value));
+
+module.exports = updateRecords;
