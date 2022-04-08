d786d68fb57a3e8f4f7820e2a5fdfdac4fea9028
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:21:54 +0200

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,16 @@
+function myRecursion(arr, n){
 
+    if (n <= 0){
+
+        return 0;
+    }
+    else {
+
+        return myRecursion(arr, n-1) + arr[n-1];
+    }
+}
+
+
+console.log(myRecursion(arr, n));
+
+module.exports = myRecursion;
