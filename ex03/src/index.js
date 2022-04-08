bdea2156f2d183e70b60d2459e25b646d86691b0
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:22:52 +0200

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,30 @@
+function rangeOfNumbers(startN, endN){
 
+    
+    if(startN > endN){
+
+        return "The starting number will always be less than or equal to the ending number";
+    }
+    else {
+
+        var diff = (endN - startN);
+        const Arr = rangeOfNumbers(diff);     
+        Arr[n] = Arr.push(startN);
+        n++;
+        if (n == diff){
+            
+            return Arr;
+        }
+        else {
+            
+            rangeOfNumbers(startN+1, endN);
+        }
+    }
+}
+
+var x = 7;
+var y = 13;
+var n = 0;
+console.log(rangeOfNumbers(x, y));
+
+module.exports = rangeOfNumbers;
