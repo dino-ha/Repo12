5c5a335eea2e29204a17c8afca6449e14f3f14fa
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:23:20 +0200

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,23 @@
+function sumFibonacci(num){
 
+    if (num < 0 || num == NaN){
+
+        return 0;
+    }
+    else {
+
+        if (num % 2 == 0){
+
+            sumFibonacci(num);
+        }
+        else {
+
+            return sumFibonacci(num-1) + sumFibonacci(num-2);
+        }
+    }
+}
+
+var num;
+sumFibonacci(num);
+
+module.exports = sumFibonacci;
