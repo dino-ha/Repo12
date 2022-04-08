5a1180288ec967199c93e6a288f31e69a5a037eb
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:24:16 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,22 @@
+function myNes(){
 
+    var myStorage = {
+
+        "car": {
+
+            "inside": {
+
+                "glove box": "maps",
+                "passenger seat": "crumbs" //TODO: kill the passengers
+            },
+            "outside": {
+
+                "trunk": "jack"
+            }
+        }
+    };
+
+    var gloveBoxContents = myStorage.car.inside["glove box"];
+    console.log(gloveBoxContents);
+    return gloveBoxContents;
+}
