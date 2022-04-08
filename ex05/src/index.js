75df0fc3ac1ab64d832b7bf8bf29ef40260cc65f
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:23:48 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,36 @@
+var myPetsArray = [
+
+    {
+
+        animalType: "Dog",
+        name: [
+
+            "Pujdo"
+        ]
+    },
+    {
+        animalType: "Cat",
+        name: [
+
+            "Maca"
+        ]
+    },
+    {
+        animalType: "Bird",
+        name: [
+
+            "Tweety"
+        ]
+    }
+];
+
+function myPetsFunction(pets){
+
+    return pets[1].name[0];
+}
+
+myPetsFunction(pets);
+
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
 
