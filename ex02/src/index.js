191e946f7c9835e6731a63c76804866561cc09c1
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 23:22:21 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,19 @@
+function myCounter(n){
 
+    if (n < 1){
+
+        return [];
+    }
+    else {
+
+        const countArray = myCounter(n-1);
+        countArray.unshift(n);
+        return countArray;
+
+    }
+}
+
+var n;
+console.log(myCounter(n));
+
+module.exports = myCounter;
